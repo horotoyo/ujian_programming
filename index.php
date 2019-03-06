bd571231e8f72f560a6f53cbb60f7b888d6d0bef
masuryo <suryowidiyantgm@gmail.com>
2019-03-06 09:13:12 +0700

    add first

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ujian TW 2 - Suryo</title>
+</head>
+<body>
+<table>
+	<thead>
+		<tr>
+			<td>No</td>
+			<td>Nama</td>
+			<td>Email</td>
+			<td>Password</td>
+		</tr>
+	</thead>
+	<tbody>
+		
+	</tbody>
+</table>
+</body>
+</html>
